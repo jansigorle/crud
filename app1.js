1dcbba58a466548e9f774c5ba61b00163b1b4abc
jansigorle <83293971+jansigorle@users.noreply.github.com>
2021-05-09 16:54:42 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -8,8 +8,8 @@ app.use(bodyparser.json());
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
-    database: 'EmployeeDB',
+    password: '',
+    database: 'employee',
     multipleStatements: true
 });
 
@@ -26,7 +26,7 @@ app.listen(3000, () => console.log('Express server is runnig at port no : 3000')
 
 //Get all employees
 app.get('/employees', (req, res) => {
-    mysqlConnection.query('SELECT * FROM Employee', (err, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM employee', (err, rows, fields) => {
         if (!err)
             res.send(rows);
         else
@@ -36,7 +36,7 @@ app.get('/employees', (req, res) => {
 
 //Get an employees
 app.get('/employees/:id', (req, res) => {
-    mysqlConnection.query('SELECT * FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
         if (!err)
             res.send(rows);
         else
@@ -44,17 +44,17 @@ app.get('/employees/:id', (req, res) => {
     })
 });
 
-//Delete an employees
-app.delete('/employees/:id', (req, res) => {
-    mysqlConnection.query('DELETE FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
-        if (!err)
-            res.send('Deleted successfully.');
-        else
-            console.log(err);
-    })
-});
+// //Delete an employees
+ app.delete('/employees/:id', (req, res) => {
+     mysqlConnection.query('DELETE FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
+         if (!err)
+             res.send('Deleted successfully.');
+         else
+             console.log(err);
+     })
+ });
 
-//Insert an employees
+// //Insert an employees
 app.post('/employees', (req, res) => {
     let emp = req.body;
     var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
@@ -71,14 +71,13 @@ app.post('/employees', (req, res) => {
 });
 
 //Update an employees
-app.put('/employees', (req, res) => {
-    let emp = req.body;
-    var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
-    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
-    mysqlConnection.query(sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary], (err, rows, fields) => {
-        if (!err)
-            res.send('Updated successfully');
-        else
-            console.log(err);
-    })
-});
+ app.put('/employees', (req, res) => {
+     let emp = req.body;
+     var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
+     CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
+     mysqlConnection.query(sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary], (err, rows, fields) => {
+         if (!err)
+             res.send('Updated successfully');
+         else
+            console.log(err);     })
+ });
